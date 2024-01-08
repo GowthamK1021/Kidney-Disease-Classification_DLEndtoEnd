ac9e593b6fd0684eb091a3864925e68775fcd9ed
GowthamK1021 <itsgowtham1021@gmail.com>
2024-01-08 21:08:01 +0530

    Preparing Base Model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
-
+from CNN_Classifier.pipeline.stage2_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -17,3 +17,14 @@ except Exception as e:
         raise e
 
 logger.info("Welcome to our Project")
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
